694e4bc2663df0677604c215031f043db2433172
Jiwomdf <mdf.jiwo@gmail.com>
2024-02-09 23:57:27 +0700

    finish project 3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,23 +7,42 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fundamental_beginner_restourant/domain/data/api/api_service.dart';
+import 'package:fundamental_beginner_restourant/domain/data/local/db_service.dart';
+import 'package:fundamental_beginner_restourant/features/main/main_screen.dart';
+import 'package:fundamental_beginner_restourant/features/main/restaurant_provider.dart';
 import 'package:fundamental_beginner_restourant/main.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  Widget createHomeScreen() =>
+      ChangeNotifierProvider<RestaurantProvider>(
+        create: (context) => RestaurantProvider(apiService: ApiService()),
+        child: MaterialApp(
+            home: MainScreen(
+              title: 'Restaurant App',
+              apiService: ApiService(),
+              dbService: DbService(),
+            )
+        ),
+      );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('expect favorite and setting icon displayed, verify return true', (WidgetTester tester) async {
+    await tester.pumpWidget(createHomeScreen());
+    expect(find.byType(IconButton), findsAtLeast(2));
+    expect(find.byIcon(Icons.star_rate_sharp), findsOneWidget);
+    expect(find.byIcon(Icons.settings), findsOneWidget);
   });
+
+  testWidgets('expect AppBar displayed, verify return true', (WidgetTester tester) async {
+    await tester.pumpWidget(createHomeScreen());
+    expect(find.byType(AppBar), findsOneWidget);
+  });
+
+  testWidgets('expect CircularProgressIndicator displayed, verify return true', (WidgetTester tester) async {
+    await tester.pumpWidget(createHomeScreen());
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
 }
