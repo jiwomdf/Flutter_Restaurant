cad070b37ddc27aa95b6e41fbae4b7ddae53d6ce
Jiwomdf <mdf.jiwo@gmail.com>
2024-02-13 17:18:42 +0700

    add json parse unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,10 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fundamental_beginner_restourant/domain/data/api/api_service.dart';
+import 'package:fundamental_beginner_restourant/domain/data/api/response/restaurant_element.dart';
 import 'package:fundamental_beginner_restourant/domain/data/local/db_service.dart';
 import 'package:fundamental_beginner_restourant/features/main/main_screen.dart';
 import 'package:fundamental_beginner_restourant/features/main/restaurant_provider.dart';
-import 'package:fundamental_beginner_restourant/main.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -28,21 +28,46 @@ void main() {
         ),
       );
 
-  testWidgets('expect favorite and setting icon displayed, verify return true', (WidgetTester tester) async {
+  late Map<String,dynamic> json;
+
+  setUp(() {
+    json = {
+      "id": "id",
+      "name": "name",
+      "description": "description",
+      "pictureId": "pictureId",
+      "city": "city",
+      "rating": 0.1.toDouble(),
+      "menus": null,
+    };
+  });
+
+  testWidgets('expect favorite and setting icon displayed, verified return true', (WidgetTester tester) async {
     await tester.pumpWidget(createHomeScreen());
     expect(find.byType(IconButton), findsAtLeast(2));
     expect(find.byIcon(Icons.star_rate_sharp), findsOneWidget);
     expect(find.byIcon(Icons.settings), findsOneWidget);
   });
 
-  testWidgets('expect AppBar displayed, verify return true', (WidgetTester tester) async {
+  testWidgets('expect AppBar displayed, verified return true', (WidgetTester tester) async {
     await tester.pumpWidget(createHomeScreen());
     expect(find.byType(AppBar), findsOneWidget);
   });
 
-  testWidgets('expect CircularProgressIndicator displayed, verify return true', (WidgetTester tester) async {
+  testWidgets('expect CircularProgressIndicator displayed, verified return true', (WidgetTester tester) async {
     await tester.pumpWidget(createHomeScreen());
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  test('Restaurant object has correct properties, verified return true', () {
+    final userData = Restaurant.fromJson(json);
+    expect(userData.id, "id");
+    expect(userData.name, "name");
+    expect(userData.description, "description");
+    expect(userData.pictureId, "pictureId");
+    expect(userData.city, "city");
+    expect(userData.rating, 0.1.toDouble());
+    expect(userData.menus, null);
   });
 
 }
